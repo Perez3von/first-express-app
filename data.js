06d6511f7bb877872749b5de45a2d082117adc7d
Evon Perez <perez.evon@gmail.com>
2021-08-11 18:31:14 -0400

    added data, req

---
+++ data.js
@@ -0,0 +1,21 @@
+ 
+const team = [
+
+{   id:23,
+    player: 'Michael Jordan'
+    
+},
+{
+    id:8,
+    player:'Kobe Bryant',
+
+},
+
+{
+    id:21,
+    player:'Lebron James'
+}
+
+]
+
+module.exports = team;
--- index.js
+++ index.js
@@ -3,16 +3,44 @@ console.log('hello world');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const data = require('./data.js');
 
 app.get('/', (req, res) => {
     res.send('hello world!!!!!!!!!')
 });
 
+
+
+
 app.get('/tears', (req, res) => {
 
-    res.send(`These are my tears from ${req.query.name}`);
+    res.json(data);
+
+    // res.send(`These are my tears from ${req.query.name}`);
 } )
+
+
+
+app.get('/tears/:id', (req, res)=>{
+
+    const teamID = Number(req.params.id);
+
+    const player =  data.find((item)=>{ 
+
+    item.id === teamID;
+
+    res.json(player)
+
+})
+
+
+})
+
+
 
 app.listen(port, () => {
     console.log(`listieing at port ${port}`)
 });
+
+
+module.exports = app;
